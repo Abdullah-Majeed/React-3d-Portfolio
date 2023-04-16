0935f7243679b23cf75c1a34813fa7243d49d180
Abdullah Majeed <abdullahmajeed969@gmail.com>
2023-04-16 18:18:09 +0500

    about section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,15 +9,13 @@ const Hero = () => {
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          {/* Ball */}
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          {/* Line */}
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Abdullah Majeed</span>
+            Hi, I'm <span className='text-[#915EFF]'>Abdullah</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className='sm:block hidden' />
@@ -28,7 +26,7 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-1 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
