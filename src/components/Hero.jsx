551497ec444d301db0b8700d57b26d856897d880
Abdullah Majeed <abdullahmajeed969@gmail.com>
2023-04-25 17:31:16 +0500

    deployed branch

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            interfaces, web and mobile applications
           </p>
         </div>
       </div>
